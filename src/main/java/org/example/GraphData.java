d93a11d4bb62ad4909ec0de79b6922b9f2c2c88e
Mathiesen <kunzmathiesen@hotmail.com>
2024-03-22 09:37:29 +0100

    Graph data added

---
+++ src/main/java/org/example/GraphData.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class GraphData {
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public double get_double() {
+        return _double;
+    }
+
+    public void set_double(double _double) {
+        this._double = _double;
+    }
+
+    private int number;
+    private String string;
+    private double _double;
+}
